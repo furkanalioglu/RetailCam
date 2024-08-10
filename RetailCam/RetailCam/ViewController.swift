156fcff082e2a1c5520a4a7a852c2dc4d927f84a
Furkan Alioglu <91939823+furkanalioglu@users.noreply.github.com>
2024-08-10 08:44:28 +0300

    initial-setup

---
+++ RetailCam/RetailCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetailCam
+//
+//  Created by Furkan Alioglu on 10.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
